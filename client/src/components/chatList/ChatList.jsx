2e04971ddd122ba7530c843b6f92aeb7586b68a0
Santhosh Kumar G <santhoshkumar.g123123@gmail.com>
2025-11-20 11:39:29 +0530

    Feat: Implement hamburger menu for mobile sidebar

--- client/src/components/chatList/ChatList.jsx
+++ client/src/components/chatList/ChatList.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import "./chatList.css";
 import { useQuery } from "@tanstack/react-query";
 
-const ChatList = () => {
+const ChatList = ({ closeMenu }) => {
   const { isPending, error, data } = useQuery({
     queryKey: ["userChats"],
     queryFn: () =>
@@ -14,18 +14,18 @@ const ChatList = () => {
   return (
     <div className="chatList">
       <span className="title">DASHBOARD</span>
-      <Link to="/dashboard">Create a new Chat</Link>
-      <Link to="/">Explore Streamify AI</Link>
-      <Link to="/">Contact</Link>
+      <Link to="/dashboard" onClick={closeMenu}>Create a new Chat</Link>
+      <Link to="/" onClick={closeMenu}>Explore Streamify AI</Link>
+      <Link to="/" onClick={closeMenu}>Contact</Link>
       <hr />
       <span className="title">RECENT CHATS</span>
       <div className="list">
         {isPending
           ? "Loading..."
           : error
-          ? "Something went wrong!"
-          : data?.map((chat) => (
-              <Link to={`/dashboard/chats/${chat._id}`} key={chat._id}>
+            ? "Something went wrong!"
+            : data?.map((chat) => (
+              <Link to={`/dashboard/chats/${chat._id}`} key={chat._id} onClick={closeMenu}>
                 {chat.title}
               </Link>
             ))}
--- client/src/layouts/dashboardLayout/DashboardLayout.jsx
+++ client/src/layouts/dashboardLayout/DashboardLayout.jsx
@@ -1,13 +1,13 @@
 import { Outlet, useNavigate } from "react-router-dom";
 import "./dashboardLayout.css";
 import { useAuth } from "@clerk/clerk-react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ChatList from "../../components/chatList/ChatList";
 
 const DashboardLayout = () => {
   const { userId, isLoaded } = useAuth();
-
   const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     if (isLoaded && !userId) {
@@ -19,7 +19,12 @@ const DashboardLayout = () => {
 
   return (
     <div className="dashboardLayout">
-      <div className="menu"><ChatList/></div>
+      <div className="menuToggle" onClick={() => setIsOpen((prev) => !prev)}>
+        <img src="/menu.png" alt="" />
+      </div>
+      <div className={`menu ${isOpen ? "active" : ""}`}>
+        <ChatList closeMenu={() => setIsOpen(false)} />
+      </div>
       <div className="content">
         <Outlet />
       </div>
